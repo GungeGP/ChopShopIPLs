598e147e06b3f77a8fc8d1006c4ddf117ea921cc
Miklas Gunge <84571847+GungeGP@users.noreply.github.com>
2023-12-27 01:27:10 +0100

    Created fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'GungeGP'
+description 'Part of the Chop Shop DLC'
+version '1.0.0'
+
+shared_scripts {
+    'Config.lua'
+}
+
+client_scripts {
+	"client/client.lua"
+}
+
+dependencies {
+    '/server:7257',
+    '/gameBuild:3095',
+}
