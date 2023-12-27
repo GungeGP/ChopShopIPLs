01a5d7be5edb81fe0579e4c373081996c96dd5e9
Miklas Gunge <84571847+GungeGP@users.noreply.github.com>
2023-12-27 01:28:31 +0100

    Created Config.lua

---
+++ Config.lua
@@ -0,0 +1,7 @@
+Config = Config or {}
+
+Config.LoadAllIpls = true -- Enable/Disable the loading of all IPLs
+Config.LoadEntitysForIpls = true -- Enable/Disable the loading of all entitys for IPLs
+
+Config.CargoShipMapRadar = true -- Enable/Disable the radar for the cargoship
+Config.CargoShipHideDoor = true -- Enable/Disable the hide door for the cargoship
